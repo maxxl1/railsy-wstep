c732a61ff539f17fdf15bde457aa5e7ae5dff0b5
maxxl <MateuszOsikowicz@gmail.com>
2015-07-14 14:48:32 +0200

    Działający system kont + powiązanie postów z konkretnym użytkownikiem

--- railsy2/blog/db/schema.rb
+++ railsy2/blog/db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708100024) do
+ActiveRecord::Schema.define(version: 20150712111819) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "nick"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
