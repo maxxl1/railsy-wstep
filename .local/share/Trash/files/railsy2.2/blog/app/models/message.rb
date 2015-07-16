e6e2fda8ea881f84f8688f4453aa226cabd8c305
maxxl <MateuszOsikowicz@gmail.com>
2015-07-16 17:53:10 +0200

    Finalne porządki na repo

--- .local/share/Trash/files/railsy2.2/blog/app/models/message.rb
+++
@@ -1,3 +0,0 @@
-class Message < ActiveRecord::Base
-validates :content, length: {minimum: 2}
-end
--- .local/share/Trash/files/railsy2.2/blog/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-  validates :nick,  presence: true, length: { maximum: 50 }, uniqueness: true
-  validates :email, presence: true, length: { maximum: 255 }
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 4 }
-end
